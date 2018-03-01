7f3542940c1215815cbd120a791e9aa03eb17498
JP <jpv1124@gmail.com>
2018-03-01 14:37:37 -0600

    .

---
+++ src/components/TaskList.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import{
+View,
+Image,
+Text,
+StyleSheet,
+TouchableHighlight
+} from 'react-native';
+
+export default class TaskList extends Component{
+constructor(props){
+super(props);
+
+}
+
+
+
+render(){
+    const {title,completed,id}=this.props.task;
+    const icon = completed ? require('./../images/icon-checked.png') : require('./../images/icon-circle.png');
+
+    return(
+      <TouchableHighlight onPress={ ()=>{ this.props.updateTask(id)}} style={styles.container}>
+        <View style={styles.taskContainer}>
+            <Image style={styles.taskIcon} source={icon}/>
+            <Text style={completed ? styles.taskTextCompleted : styles.taskText}>{title}</Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container:{
+    height:60
+  },
+  taskIcon: {
+    height: 25,
+    width: 25,
+    marginRight: 15
+  },
+  taskText: {
+    fontSize: 21
+  },
+  taskTextCompleted:{
+    fontSize:21,
+    color: '#A4A4A4'
+  },
+  taskContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 10,
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderColor: '#E7E7E7'
+  }
+});
---
+++ src/components/TasksHeader.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import{
+View,
+Image,
+Text,
+StyleSheet,
+ImageBackground
+} from 'react-native';
+
+export default class TasksHeader extends Component{
+constructor(props){
+super(props);
+
+}
+
+generateDate(){
+
+  const daysOfTheWeek = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'],
+    months =['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+
+
+   const currentDate = new Date(),
+    day = daysOfTheWeek[currentDate.getDay()],
+    month = months[currentDate.getMonth()],
+    year = currentDate.getFullYear(),
+    date = currentDate.getDate();
+
+
+   return `${day} ${date} de ${month} ${year}`.toUpperCase;
+
+}
+
+render(){
+   return(
+     <View style={ styles.container }>
+       <ImageBackground style={styles.backgroundImage} source={require('./../images/tasks-bg.jpg')}>
+         <Image style={styles.userAvatar} source={require('./../images/user-avatar.png')}/>
+         <Text style={styles.pendingTasksText}>{this.props.toBeCompleted} Pendientes</Text>
+         <Text style={styles.dateText}>{this.generateDate()}</Text>
+       </ImageBackground>
+     </View>
+   )
+ }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  userAvatar: {
+    height: 130,
+    width: 130,
+    borderRadius: 65
+  },
+  pendingTasksText: {
+    marginTop: 25,
+    fontSize: 36,
+    color: 'white'
+  },
+  dateText: {
+    marginTop: 8,
+    fontSize: 16,
+    color: '#A0A0A0'
+  }
+})
